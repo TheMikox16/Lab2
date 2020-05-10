3cff66ffb56516bd7ec6922b2551d533af34cbcd
TheMikox16 <miguel.egoavil@ucrso.info>
2020-05-10 11:58:04 -0600

    Finalizado las pruebas y el laboratorio

--- Lab2/src/excercise1/purchaseapp/Main.java
+++ Lab2/src/excercise1/purchaseapp/Main.java
@@ -11,6 +11,7 @@ import excercise1.patterns.FragilWrap;
 import excercise1.patterns.GiftCard;
 import excercise1.patterns.GiftWrap;
 import excercise1.patterns.IShipments;
+import excercise1.patterns.ShipmentByBoat;
 import excercise1.patterns.ShipmentByPlane;
 import excercise1.patterns.WarehouseStaff;
 import excercise1.purchaseapp.DeliveredList;
@@ -48,6 +49,8 @@ public class Main {
         p1.addProduct(new Product("213", 1, 5.7));
         p1.addProduct(new Product("111", 5, 0.7));
         
+        p1.removeProduct("111");
+        
         p1.setShipping(new ShipmentByPlane());
         ShipmentByPlane shipmentPlane = new ShipmentByPlane();
         IShipments ship = new BasicShipment();
@@ -55,12 +58,6 @@ public class Main {
         IShipments giftDecorator1 = new GiftCard(ship);
         giftDecorator.shipmentPlaneDecorator(shipmentPlane);
         giftDecorator1.shipmentPlaneDecorator(shipmentPlane);
-        p1.shipping();
-        
-        shipmentPlane.print(p1);
-        
-        
-        p1.removeProduct("111");
                 
         Purchase p2 = new Purchase();
         p2.addPurchaseObserver(new Buyer());
@@ -70,6 +67,14 @@ public class Main {
         p2.addProduct(new Product("432", 2, 7.6));
         p2.addProduct(new Product("456", 6, 10.1));
         p2.addProduct(new Product("654", 3, 1.2));
+        
+        p2.setShipping(new ShipmentByBoat());
+        ShipmentByBoat shipmentBoat = new ShipmentByBoat();
+        ship = new BasicShipment();
+        giftDecorator = new GiftWrap(ship);
+        giftDecorator1 = new GiftCard(ship);
+        giftDecorator.shipmentBoatDecorator(shipmentBoat);
+        giftDecorator1.shipmentBoatDecorator(shipmentBoat);
                 
         Purchase p3 = new Purchase();
         p3.addPurchaseObserver(new Buyer());
@@ -80,6 +85,14 @@ public class Main {
         p3.addProduct(new Product("867", 7, 9.4));
         p3.addProduct(new Product("866", 2, 8.7));
         
+        p3.setShipping(new ShipmentByBoat());
+        shipmentBoat = new ShipmentByBoat();
+        ship = new BasicShipment();
+        giftDecorator = new GiftWrap(ship);
+        giftDecorator1 = new FragilWrap(ship);
+        giftDecorator.shipmentBoatDecorator(shipmentBoat);
+        giftDecorator1.shipmentBoatDecorator(shipmentBoat);
+        
         Purchase p4 = new Purchase();
         p4.addPurchaseObserver(new Buyer());
         p4.addPurchaseObserver(staff);
@@ -88,6 +101,16 @@ public class Main {
         p4.addProduct(new Product("876", 9, 1.3));
         p4.addProduct(new Product("321", 4, 9.0));
         p4.addProduct(new Product("123", 2, 2.6));
+        
+        p4.setShipping(new ShipmentByPlane());
+        shipmentPlane = new ShipmentByPlane();
+        ship = new BasicShipment();
+        giftDecorator = new FragilWrap(ship);
+        giftDecorator1 = new GiftCard(ship);
+        IShipments giftDecorator2 = new GiftWrap(ship);
+        giftDecorator.shipmentPlaneDecorator(shipmentPlane);
+        giftDecorator1.shipmentPlaneDecorator(shipmentPlane);
+        giftDecorator2.shipmentPlaneDecorator(shipmentPlane);
         
         processing.addPurchase(p2);
         Thread.sleep(200);
@@ -129,9 +152,28 @@ public class Main {
         System.out.println("Lista de entregados por status:\n\n" 
                 + delivered.purchasesByStatusDate());
         
+        System.out.println("Compra 1:\n");
+        p1.printObservers();
+        System.out.println("Compra 4:\n");
         p4.printObservers();
+      
+        processing.printObservers();  
         
-        processing.printObservers();
+        System.out.print("Detalles envio compra 1:\n");
+        shipmentPlane.print(p1);
+        p1.shipping();
+
+        System.out.print("\n\nDetalles envio compra 2:\n");
+        shipmentBoat.print(p2);
+        p2.shipping();
+        
+        System.out.print("\n\nDetalles envio compra 3:\n");
+        shipmentBoat.print(p3);
+        p3.shipping();
+        
+        System.out.print("\n\nDetalles envio compra 4:\n");
+        shipmentPlane.print(p4);
+        p4.shipping();
         
         }catch (InterruptedException ex){
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
